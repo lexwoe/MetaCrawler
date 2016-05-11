855033dafc74fc7dc2586d45a6a05d1673bd519b
lexwoe <lexwoe@126.com>
2016-05-11 20:54:16 +0800

    Create Crawler.java
    This class is used for gathering all of resources for crawling web pages.

---
+++ idio/crawler/Crawler.java
@@ -0,0 +1,106 @@
+package idio.crawler;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import idio.handler.Handler;
+import idio.handler.HandlerInfor;
+
+/**
+ * 
+ * @author lexwoe
+ *
+ * This class is used for gathering all of resources for
+ * crawling web pages. 
+ *
+ * @param <O> : Any Object, e.g. Box, even String etc.
+ * 
+ * @version 16.05
+ */
+
+public class Crawler<O> {
+
+	private List<String> seeds;
+	private Handler<O> hd;
+	private ArrayList<O> boxes;
+
+	public Crawler() {
+
+	}
+
+	public Crawler(List<String> seeds) {
+		this.seeds = seeds;
+	}
+
+	public Crawler(HandlerInfor<O> hd) {
+		this.hd = hd;
+	}
+
+	public Crawler(List<String> seeds, Handler<O> hd) {
+		this.seeds = seeds;
+		this.hd = hd;
+	}
+
+	/**
+	 * Visit all of seeds
+	 * 
+	 * @throws Exception
+	 */
+	public void run() throws Exception {
+		this.boxes = new ArrayList<O>();
+		for (String url : this.seeds) {
+			if (touch(new URL(url)))
+				this.visit(new URL(url));
+		}
+	}
+
+	/**
+	 * Go to seed
+	 * 
+	 * @param url
+	 * @throws Exception
+	 */
+	public void visit(URL url) throws Exception {
+		this.hd.handleURL(url);
+		if (this.hd.getBoxes() != null)
+			this.addBox(this.hd.getBoxes());
+	}
+
+	/**
+	 * Should visit ? 
+	 * Or give an action here
+	 * 
+	 * @param url
+	 * @return
+	 */
+	public boolean touch(URL url) {
+		return true;
+	}
+
+	/**
+	 * Add results ## you can override ##
+	 * 
+	 * @return
+	 */
+	public void addBox(ArrayList<O> boxes) {
+		this.boxes.addAll(boxes);
+	}
+
+	public List<String> getSeeds() {
+		return seeds;
+	}
+
+	public void setSeeds(List<String> seeds) {
+		this.seeds = seeds;
+	}
+
+	public ArrayList<O> getBoxes() {
+		return boxes;
+	}
+
+	public void setBoxes(ArrayList<O> boxes) {
+		this.boxes = boxes;
+	}
+
+}
