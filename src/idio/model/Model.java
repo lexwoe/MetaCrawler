8ccbbe1f003d0a8a8ac950cbc319e3241350125b
lexwoe <lexwoe@126.com>
2016-05-17 22:40:02 +0800

    Add files via upload

--- src/idio/model/Model.java
+++ src/idio/model/Model.java
@@ -1,7 +1,5 @@
 package idio.model;
 
-import java.util.regex.Pattern;
-
 import idio.beans.Pattern2Box;
 
 /**
@@ -15,23 +13,11 @@ import idio.beans.Pattern2Box;
 
 public class Model {
 	
-	/**
-	 * some patterns for locating targets
-	 */
-	public static final Pattern HTML_H_S = Pattern.compile("<h\\d+\\s*.*?>(.*)", Pattern.CASE_INSENSITIVE);
-	public static final Pattern HTML_H_E = Pattern.compile("(.*)</h\\d+>", Pattern.CASE_INSENSITIVE);
-	public static final Pattern HTML_H34_S = Pattern.compile("<h[34]\\s*.*?>(.*)", Pattern.CASE_INSENSITIVE);
-	public static final Pattern HTML_H34_E = Pattern.compile("(.*)</h[34]>", Pattern.CASE_INSENSITIVE);
-	public static final Pattern HTML_P_S = Pattern.compile("<p\\s*.*?>(.*)", Pattern.CASE_INSENSITIVE);
-	public static final Pattern HTML_P_E = Pattern.compile("(.*)</p>", Pattern.CASE_INSENSITIVE);
-	public static final Pattern HTML_D_S = Pattern.compile("<div\\s*.*?>(.*)", Pattern.CASE_INSENSITIVE);
-	public static final Pattern HTML_D_E = Pattern.compile("(.*)</div>", Pattern.CASE_INSENSITIVE);
+	public static final String M_MAY = "MAY";
+	public static final String M_MUST = "MUST";
 
 	// patterns
 	private Pattern2Box[] pats;
-	
-	// allow targeting or not
-	private boolean flag = false;
 	
 	public void loadPattern(Pattern2Box[] pats){
 		this.pats = pats;
@@ -43,15 +29,6 @@ public class Model {
 	 * @return
 	 */
 	public boolean detect(String line){
-		if(pats!=null){
-			// detect starting marks of target
-			if (this.pats[0].getP_s()!=null && this.pats[0].getP_s().matcher(line).find()) {
-				this.flag = true;
-				return true;
-			// detect ending marks of target
-			} else if (this.pats[0].getP_e()!=null && this.pats[0].getP_e().matcher(line).find())
-				this.flag = false;
-		}
 		return false;
 	}
 
@@ -64,12 +41,7 @@ public class Model {
 	}
 
 	public boolean allow() {
-		return flag;
+		return true;
 	}
-
-	public void setAllow(boolean flag) {
-		this.flag = flag;
-	}
-	
 	
 }
